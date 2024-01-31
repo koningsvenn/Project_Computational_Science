4dad2129cb34acf4ddf121f9fb02e94f5cd76b76
iris9812 <iris.mittel@gmail.com>
2024-01-31 10:19:56 +0100

    validation plots

---
+++ validation.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np 
+
+#load data
+rain = pd.read_csv('validation_data/rainfall_climate_knowledge_portal.csv', delimiter=';')
+humidity = pd.read_csv('validation_data/humidity.csv', delimiter=';')
+
+#plot humidity vs rainfall per season
+seasons = ['Summer', 'Fall', 'Winter', 'Spring']
+
+#start subpolots
+fig, axs = plt.subplots(2, 2, figsize=(22, 12))
+fig.suptitle(f'Rainfall for different humidities per season', y =0.95)
+
+for season, ax in zip(seasons, axs.ravel()):
+    ax.scatter(humidity[season], rain[season], label=season)
+    ax.set_xlabel('relative humidity')
+    ax.set_ylabel('rainfall [mm]')
+    ax.set_title(season)
+
+    #best fit
+    a, b = np.polyfit(humidity[season], rain[season], 1)
+    ax.plot(humidity[season], a*humidity[season]+b, linestyle='--', linewidth=2)
+
+plt.savefig(f'figures/validation')
